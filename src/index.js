82299d422c9a989c0ebd2cf67fcc63fbfd6ff320
nick <morri2ne@dukes.jmu.edu>
2020-05-26 13:59:33 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App.jsx";
 import playGame from "./phaser/scene";
+import './css/main.css';
 
 //console.log(App);
 
